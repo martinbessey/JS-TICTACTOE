3cb40963377a44052621745fef05bf961ec21f18
Martin BESSEY <77329079+martinbessey@users.noreply.github.com>
2021-02-22 17:37:57 +0100

    Add files via upload

---
+++ appmorpion.js
@@ -0,0 +1,107 @@
+const player1 = "X";
+const player2 = "O";
+let currentPlayer = player1;
+let victory = false;
+let counter = 0;
+
+let score;
+let cells = document.getElementsByTagName("td");
+let msg = document.getElementById("message");
+msg.innerHTML = "Let's go " + player1 + " !";
+
+let state = [];
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
+for (let i = 0; i < cells.length; i++) {
+  let cell = cells[i];
+  cell.addEventListener("click", () => {
+    if (cell.innerHTML == "" && victory == false) {
+      counter++;
+      cell.innerHTML = currentPlayer;
+      state[i] = cell.innerHTML;
+      if (counter >= 5) {
+        checkVictory(state)
+      }
+      if (victory == true) {
+        msg.innerHTML =
+          "Congratulations " +
+          currentPlayer+" !" +
+          "<br><a href=''>New Game</a>";
+        if (currentPlayer == player1) {
+          scorePlayer1++;
+          document.getElementById("score-player-1").innerHTML = ("Score: +"+scorePlayer1);
+        }
+        if (currentPlayer == player2) {
+          scorePlayer2++;
+          document.getElementById("score-player-2").innerHTML = ("Score: +"+scorePlayer2);
+        }
+      } else if (counter == 9 && victory == false) {
+        msg.innerHTML = "DRAW !!!<br><a href=''>New Game</a>";
+      } else {
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+        msg.innerHTML = "Let's go " + currentPlayer + " !";
+      }
+    }
+  });
+}
+
+function checkVictory(state) {
+    if (
+      state[0] == currentPlayer &&
+      state[0] == state[1] &&
+      state[1] == state[2]
+    ) {
+      victory = true;
+    }
+    if (
+      state[3] == currentPlayer &&
+      state[3] == state[4] &&
+      state[4] == state[5]
+    ) {
+      victory = true;
+    }
+    if (
+      state[6] == currentPlayer &&
+      state[6] == state[7] &&
+      state[7] == state[8]
+    ) {
+      victory = true;
+    }
+    if (
+      state[0] == currentPlayer &&
+      state[0] == state[3] &&
+      state[3] == state[6]
+    ) {
+      victory = true;
+    }
+    if (
+      state[1] == currentPlayer &&
+      state[1] == state[4] &&
+      state[4] == state[7]
+    ) {
+      victory = true;
+    }
+    if (
+      state[2] == currentPlayer &&
+      state[2] == state[5] &&
+      state[5] == state[8]
+    ) {
+      victory = true;
+    }
+    if (
+      state[0] == currentPlayer &&
+      state[0] == state[4] &&
+      state[4] == state[8]
+    ) {
+      victory = true;
+    }
+    if (
+      state[2] == currentPlayer &&
+      state[2] == state[4] &&
+      state[4] == state[6]
+    ) {
+      victory = true;
+    }
+    return false;
+  }
